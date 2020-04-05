3fbb06a976af2bf5f2f1d158dbbe4ead27a0085d
Mikhail Katsupko <t63723@gmail.com>
2020-04-05 13:59:39 +0700

    Updates

---
+++ precache-manifest.d77a2d6323dc0b1af21a21cfb96578e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa886adc1a3847f2a9ee2f81eb2e008",
+    "url": "/t63723/vk-app-ft/index.html"
+  },
+  {
+    "revision": "1e62b013b00370399aa3",
+    "url": "/t63723/vk-app-ft/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "c635a2823c275cdc4c22",
+    "url": "/t63723/vk-app-ft/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1e62b013b00370399aa3",
+    "url": "/t63723/vk-app-ft/static/js/2.7e11aefa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t63723/vk-app-ft/static/js/2.7e11aefa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c635a2823c275cdc4c22",
+    "url": "/t63723/vk-app-ft/static/js/main.d2b5fb65.chunk.js"
+  },
+  {
+    "revision": "c06642f4ad18ff3b218e",
+    "url": "/t63723/vk-app-ft/static/js/runtime-main.2396db3e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/t63723/vk-app-ft/static/media/persik.4e1ec840.png"
+  }
+]);
